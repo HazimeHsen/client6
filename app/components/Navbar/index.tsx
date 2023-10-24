1464da87e58ed45d6a705352d0be16124a68a909
Hsen-123 <hazimehussein01@gmail.com>
2023-10-24 19:54:51 +0300

    first commit

---
+++ app/components/Navbar/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "../header";
+import Nav from "./Nav";
+const index = () => {
+  return (
+    <div>
+      <Nav />
+    </div>
+  );
+};
+
+export default index;
---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+import Hero from "./components/Hero";
+import IconsEveryWhere from "./components/IconsEveryWhere";
+import WhatWeDo from "./components/WhatWeDo/WhatWeDo";
+import About from "./components/About";
+import LogoSlideShow from "./components/logoSlideShow/LogoSlideShow";
+
+export default function Home() {
+  return (
+    <div className="">
+      <Hero />
+      <LogoSlideShow />
+      <WhatWeDo />
+      <IconsEveryWhere />
+    </div>
+  );
+}
